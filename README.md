0846fcd4eaa3156ca843cd95c41d100bec67b2ca
DEVICARUS <me@devicarus.eu>
2019-06-14 14:37:54 +0200

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
 markpdf
+
+> Feature-rich markdown convertor currently supporting HTML and PDF
+
+```bash
+npm i markconv
+```
+
+## Features
+
+- CSS support
+
+- Page break helper
+
+- Automatic Table of Contents generation
+
+- Automatic wiki-like heading numbering
+
+- LaTeX support
+
+## How to use
+
+### CLI
+
+Install `markconv` globaly
+
+```bash
+npm i markconv -g
+```
+
+Use `markconf`
+
+```bash
+markconv [source] [style] [format]
+```
+
+`source` - Path to your markdown file *(required)*
+
+`style` - Path to your CSS file *(required, maybe changed in later updates)*
+
+`format` - Your target format *(required)*
+
+### Code
+
+Install `markconv`
+
+```
+npm i markconv
+```
+
+Use `markconv` in your code
+
+```javascript
+markpdf({
+    source: "source.md", // Path to your markdown file (required)
+    style: "style.css", //Path to your CSS file (required, maybe changed in later updates)
+    output: "format" // Your target format (required)
+}).then(output => {
+    // YOUR CODE
+})
+```
--- cli.js
+++ cli.js
@@ -1,19 +1,84 @@
 #! /usr/bin/env node
 
-var markpdf = require('./lib/main')
+'use strict'
+var markpdf = require('./main')
 
+const meow = require('meow');
 var fs = require('fs')
 var replaceExt = require('replace-ext');
 
-var source = process.argv[2],
-    style = process.argv[3],
-    output = process.argv[4]
+const cli = meow(`
+	Usage
+	  $ markconf source=<source file> style=<styles file (optional)> format=<output format> config=<config file> [Options]
 
-markpdf(source, style, output)
-.then((file) => {
-    var target = replaceExt(source, `.${output}`)
-    fs.writeFileSync(target, file, "utf-8")
-    console.log(`Successfuly exported to ${output}`)
-}).catch(err => {
-    console.log(err)
-})
+	Supported output formats
+	  html, pdf
+ 
+	Options
+	  --nosandbox, -ns  Passes no-sandbox flag to Puppeteer
+ 
+	Examples
+	  $ markconf source=MyFile.md style=style.css format=pdf --nosandbox
+`, {
+	flags: {
+		nosandbox: {
+			type: 'boolean',
+			alias: 'ns'
+		},
+	}
+});
+
+var parameters = {
+	'source': "",
+	'style': "",
+	'output': "",
+	'config': ""
+}
+
+for (let index = 0; index < cli.input.length; index++) {
+	var argument = cli.input[index].split('=')
+
+	switch (argument[0]) {
+		case 'source':
+			parameters.source = argument[1]
+			break;
+
+		case 'style':
+			parameters.style = argument[1]
+			break;
+
+		case 'format':
+			parameters.output = argument[1]
+			break;
+
+		case 'config':
+			parameters.config = argument[1]
+			break;
+
+		default:
+			console.log(new Error("Unknown parameter " + argument[0]))
+			break;
+	}
+}
+
+var preferences = {
+	pdf: {
+		config: null,
+		arguments: {
+			sandbox: cli.flags.sandbox
+		}
+	}
+}
+
+if (parameters.config != "") {
+	preferences.pdf.config = require(path.resolve(parameters.config));	
+}
+
+markpdf(parameters, preferences)
+	.then((file) => {
+		var target = replaceExt(parameters.source, `.${parameters.output}`)
+		fs.writeFileSync(target, file, "utf-8")
+		console.log(`Successfuly exported to ${parameters.output}`)
+	}).catch(err => {
+		console.log(err)
+	})
